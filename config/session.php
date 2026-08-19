ece95c0daafdfb4bee1a8a912e0630b12e0fccca
wistone-code <wistonebeno@gmail.com>
2026-08-19 15:35:55 +0300

    Lower session timeout to 30 minutes, add composer.lock

--- config/session.php
+++ config/session.php
@@ -6,7 +6,7 @@ return [
 
     'driver' => env('SESSION_DRIVER', 'database'),
 
-    'lifetime' => (int) env('SESSION_LIFETIME', 120),
+    'lifetime' => (int) env('SESSION_LIFETIME', 30),
 
     'expire_on_close' => env('SESSION_EXPIRE_ON_CLOSE', false),
 
