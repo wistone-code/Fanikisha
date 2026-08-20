25e12f62de0680e9dd1e4cb4df7250af5f9e14f1
wistone-code <wistonebeno@gmail.com>
2026-08-20 14:12:04 +0300

    Revert budget variance back to budget minus collected

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -90,7 +90,7 @@ class Event extends Model
             'collected' => (float) $collected,
             'remain' => (float) ($totalPledged - $collected),
             'budget' => (float) $budget,
-            'variance' => (float) ($budget - $totalPledged),
+            'variance' => (float) ($budget - $collected),
             'pledge_count' => $this->pledges()->count(),
         ];
     }
